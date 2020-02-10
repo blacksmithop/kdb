c7555685268367e33b77218f01127b1ab898511b
ambu <60320192+blacksmithop@users.noreply.github.com>
2020-02-10 13:08:03 +0530

    Add files via upload

---
+++ cogs/chatter.py
@@ -0,0 +1,37 @@
+import json
+from random import choice
+from discord.ext import commands
+import discord
+
+with open('cogs/responses.json', 'r') as r:
+    responses = json.load(r)['responses']
+with open('cogs/shorthand.json', 'r') as r:
+    shorthand = json.load(r)['shorthand']
+
+
+class solla(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='?', pass_context=True)
+    @commands.guild_only()
+    async def parachil(self, ctx):
+        await ctx.send(choice(responses))
+
+    @commands.command(name='!', pass_context=True)
+    @commands.guild_only()
+    async def respond(self, ctx,arg):
+        member = ctx.author
+        channel = ctx.channel
+        '''
+        while True:
+            async for msg in channel.history(limit=1):
+                if shorthand[arg]:
+                    if msg.content == 'stop' or msg.content == 'mathi':
+                        break
+                    else:'''
+        await channel.send(shorthand[arg])
+
+
+def setup(bot):
+    bot.add_cog(solla(bot))
