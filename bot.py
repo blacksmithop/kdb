0e91bb3f824422e20f306e38202371b70fda6ffa
Ikriyo <blacksmithop>
2020-03-06 20:20:21 +0530

    add ascii to startup cogs

--- bot.py
+++ bot.py
@@ -1,14 +1,19 @@
-from discord.ext import commands
+
 from os import environ
+
+from typing import List
+
+from discord.ext import commands
 
 bot = commands.AutoShardedBot(command_prefix='.', pm_help=None, description='bot', shard_count=4)
 
-initial_extensions = ["cogs.admin", "cogs.fox", "cogs.chatter"]
+initial_extensions: List[str] = ["cogs.admin", "cogs.fox", "cogs.chatter", "cogs.ascii"]
 
 
 def initlialize():
     for extension in initial_extensions:
         bot.load_extension(extension)
+
 
 initlialize()
 
@@ -37,12 +42,9 @@ async def on_member_join(member):
     await channel.send(f"💪😎 {member.name} has joined, vanakkam")
 
 
-
 @bot.event
 async def on_member_leave(member):
     channel = bot.get_channel(620985606042026005)
     await channel.send(f"{member.name} has left, sed aayi")
 
-
 bot.run(environ['DISCORD_TOKEN'])
-
