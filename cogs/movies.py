97e689720b4bb0b49b781731c35afaaa2ad304ae
Ikriyo <blacksmithop>
2020-03-22 07:16:48 +0530

    Fixed code in movies.py

--- cogs/movies.py
+++ cogs/movies.py
@@ -1,9 +1,11 @@
+
 from imdb import IMDb
 from discord import Embed
-from  re import search
-
+from re import search
 
 from discord.ext import commands
+from google_images_download import google_images_download
+
 
 class movies(commands.Cog):
     def __init__(self, bot):
@@ -11,10 +13,11 @@ class movies(commands.Cog):
 
     @commands.command(name='movie', aliases=['mov'])
     @commands.guild_only()
-    async def get_movie(self, ctx,args):
+    async def get_movie(self, ctx, args):
         ia = IMDb()
+        response = google_images_download.googleimagesdownload()
         movie_embed = Embed()
-        movie_embed = handle(args,ia,movie_embed)
+        movie_embed = handle(args, ia, movie_embed,response)
         await ctx.send(embed=movie_embed)
 
 
@@ -22,9 +25,7 @@ def setup(bot):
     bot.add_cog(movies(bot))
 
 
-
-
-def handle(movie_name, ia, movie_embed):
+def handle(movie_name, ia, movie_embed,response):
     # say('Searching top five results for.  %s' %movie_name)
     movie_query = ia.search_movie(movie_name)
     del movie_query[5:]
@@ -51,12 +52,12 @@ def handle(movie_name, ia, movie_embed):
             info = search(r'name:(.*?)>', info).group(1)
             info = f"Directed by: {info}"
             movie_embed.add_field(name="\u200b", value=info, inline=False)
-        #if movie.get('producer'):
-            #info = f"Producers.  {movie.get('producer')}  "
-            #movie_embed.add_field(name="\u200b", value=info, inline=False)
-        #if movie.get('cast'):
-           # #info = f"Cast.  {movie.get('cast')}"
-            #movie_embed.add_field(name="\u200b", value=info, inline=False)
+        # if movie.get('producer'):
+        # info = f"Producers.  {movie.get('producer')}  "
+        # movie_embed.add_field(name="\u200b", value=info, inline=False)
+        # if movie.get('cast'):
+        # #info = f"Cast.  {movie.get('cast')}"
+        # movie_embed.add_field(name="\u200b", value=info, inline=False)
         break
     return movie_embed
 
