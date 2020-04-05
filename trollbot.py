300ed0d3b4009f48e094f9f6a002419c119d5091
ambu <60320192+blacksmithop@users.noreply.github.com>
2020-04-05 15:34:08 +0530

    Create trollbot.py

---
+++ trollbot.py
@@ -0,0 +1,25 @@
+from os import environ
+from discord.ext import commands
+from random import choice
+
+bot = commands.AutoShardedBot(command_prefix='..', pm_help=None, description='bot', shard_count=1)
+
+
+@bot.event
+async def on_message(message):
+    emoji = [
+        "😀", "😛", "😈", "👻", "💩", "😸", "🕸", "⚽", "🥇", "🧿", "🧲", "📳", "🎥", "📕", "🖋",
+        "⏰", "🍔", "🌭", "🍿", "🥨", "🍛", "🍧", "🚓", "🚗", "🦽", "🛹", "🚲", "⚓", "☮", "🆎", "🔞", "✅"
+    ]
+
+    mentioned = message.mentions
+
+    for i in mentioned:
+        if str(i) == 'abhi#1571':
+            await message.add_reaction(choice(emoji))
+            break
+
+    if 'abhi' in message.content or "ABHI" in message.content:
+        await message.add_reaction(choice(emoji))
+
+bot.run(environ['DISCORD_TOKEN'])
