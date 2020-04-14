1172553d25affc0e0cffebfb80e34c276f837a1d
ambu <60320192+blacksmithop@users.noreply.github.com>
2020-04-14 16:05:56 +0530

    Add files via upload

--- cogs/chatter.py
+++ cogs/chatter.py
@@ -3,12 +3,15 @@ from random import choice
 from discord.ext import commands
 import discord
 from googletrans import Translator
+
 translator = Translator()
 
 with open('cogs/responses.json', 'r') as r:
     responses = json.load(r)['responses']
- with open('cogs/english.txt', 'r') as r:
+
+with open('cogs/english.json', 'r') as r:
     english = json.load(r)['responses']
+
 with open('cogs/shorthand.json', 'r') as r:
     shorthand = json.load(r)['shorthand']
 
@@ -21,7 +24,7 @@ class solla(commands.Cog):
     @commands.guild_only()
     async def parachil(self, ctx):
         await ctx.send(choice(responses))
-        
+
     @commands.command(name='??', pass_context=True)
     @commands.guild_only()
     async def talker(self, ctx):
@@ -29,22 +32,22 @@ class solla(commands.Cog):
 
     @commands.command(name='!', pass_context=True)
     @commands.guild_only()
-    async def respond(self, ctx,arg):
+    async def respond(self, ctx, arg):
         channel = ctx.channel
         await channel.send(shorthand[arg])
 
     @commands.command(name='m', pass_context=True)
     @commands.guild_only()
-    async def mtrans(self, ctx,*,arg):
+    async def mtrans(self, ctx, *, arg):
         mal = translator.translate(arg, dest="ml")
         await ctx.send(mal.text)
-    
+
     @commands.command(name='h', pass_context=True)
     @commands.guild_only()
-    async def htrans(self, ctx,*,arg):
+    async def htrans(self, ctx, *, arg):
         hin = translator.translate(arg, dest="hi")
         await ctx.send(hin.text)
-        
+
 
 def setup(bot):
     bot.add_cog(solla(bot))
